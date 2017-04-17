ff5b4d6bcaec122d78166733ba73676e43bc05bb
keisatou <dummy>
2017-04-18 00:20:14 +0900

    Only display the Form if it has NOT been submitted
    Display the Data Summary ONCE the Form HAS been submitted

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <form>
+    <form v-if="!isSubmitted">
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <!-- Exercise 1 -->
@@ -9,46 +9,6 @@
           <!-- Mail -->
           <!-- Password -->
           <!-- Store Data? Yes/No -->
-          <div class="form-group">
-            <label for="fullname">Full Name</label>
-            <input
-            id="fullname"
-            class="form-control"
-            type="text"
-            v-model="fullname">
-          </div>
-          <div class="form-group">
-            <label for="email">Email</label>
-            <input
-            id="email"
-            class="form-control"
-            type="text"
-            v-model="email">
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input
-            id="password"
-            class="form-control"
-            type="password"
-            v-model="password">
-          </div>
-          <div class="form-group">
-            <label for="storeyes" class="radio-inline">
-              <input
-              type="radio"
-              id="storeyes"
-              value="yes"
-              v-model="storedata">Yes
-            </label>
-            <label for="storeno" class="radio-inline">
-              <input
-              type="radio"
-              id="storeno"
-              value="no"
-              v-model="storedata">No
-            </label>
-          </div>
 
           <!-- Exercise 2 -->
           <!-- Only display the Form if it has NOT been submitted -->
@@ -57,11 +17,58 @@
           <!-- Exercise 3 -->
           <!-- Edit the Example from above and create a custom "Full Name" Control -->
           <!-- which still holds the First Name and Last Name Input Field -->
+          <div class="form-group">
+            <label for="fullname">Full Name</label>
+            <input
+                   id="fullname"
+                   class="form-control"
+                   type="text"
+                   v-model="fullname">
+          </div>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input
+                   id="email"
+                   class="form-control"
+                   type="text"
+                   v-model="email">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+                   id="password"
+                   class="form-control"
+                   type="password"
+                   v-model="password">
+          </div>
+          <div class="form-group">
+            <label for="storeyes" class="radio-inline">
+              <input
+                                  type="radio"
+                                  id="storeyes"
+                                  value="yes"
+                                  v-model="storedata">Yes
+            </label>
+            <label for="storeno" class="radio-inline">
+              <input
+                                 type="radio"
+                                 id="storeno"
+                                 value="no"
+                                 v-model="storedata">No
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <button
+            class="btn btn-primary"
+            @click.prevent="submitted">Submit</button>
         </div>
       </div>
     </form>
     <hr>
-    <div class="row">
+    <div class="row" v-if="isSubmitted">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -87,7 +94,13 @@ export default {
       email: '',
       password: '',
       storedata: 'yes',
+      isSubmitted: false,
     };
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = true;
+    },
   },
 };
 </script>
